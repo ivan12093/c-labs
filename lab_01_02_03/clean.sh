8913db2ac50f3ae21bfeb029a4ab7931429311aa
Bulgakov Ivan <bulgakovis@student.bmstu.ru>
2022-03-10 22:34:45 +0300

    Fixed clean.sh.

--- lab_01_02_03/clean.sh
+++ lab_01_02_03/clean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-to_delete=$(find . -type f -regex ".*\.\(exe\|o\|gcov\|gcna\|gcno\)")
+to_delete=$(find . -type f -regex ".*\.\(exe\|o\|gcov\|gcda\|gcno\)")
 
 for file in $to_delete; do
 	rm "$file"	
