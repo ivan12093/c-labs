e578b9107d133c697f5d9a99cc0c4c44cb4bd0de
Bulgakov Ivan <bulgakovis@student.bmstu.ru>
2022-03-28 22:08:35 +0300

    Fixed 1st task.

--- lab_03_01_03/main.c
+++ lab_03_01_03/main.c
@@ -21,17 +21,21 @@ bool is_monotonic(const int array[], size_t array_size)
 
     // sign shows what sequence do: decreasing -1, increasing 1, equality - 0 
     int sign = (array[1] - array[0] > 0) - (array[1] - array[0] < 0);
+    int new_sign;
+
     for (size_t i = 1; i < array_size - 1; ++i)
     {
-        int new_sign = (array[i + 1] - array[i] > 0) - (array[i + 1] - array[i] < 0);
+        new_sign = (array[i + 1] - array[i] > 0) - (array[i + 1] - array[i] < 0);
 
-        if (new_sign != sign)
-            return false;
-        
-        sign = new_sign;
+        if (new_sign)
+        {
+            if (new_sign != sign)
+                return false;
+            sign = new_sign;
+        }
     }
 
-    return true;
+    return (!new_sign || sign == new_sign);
 }
 
 
@@ -77,6 +81,7 @@ int main(void)
     for (size_t i = 0; i < rows; ++i)
         mask[i] = is_monotonic(matrix[i], columns);
 
+    printf("Mask for matrix:\n");
     print_array(mask, rows);
     return EXIT_SUCCESS;
 }
